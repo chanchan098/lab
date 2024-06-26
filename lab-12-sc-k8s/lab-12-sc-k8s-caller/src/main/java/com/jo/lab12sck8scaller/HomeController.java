608866d248578a6522d9e3ad1919346f99505aa5
unknown <unknown@mail.com>
2024-06-26 16:26:45 +0800

    - add caller.

---
+++ lab-12-sc-k8s/lab-12-sc-k8s-caller/src/main/java/com/jo/lab12sck8scaller/HomeController.java
@@ -0,0 +1,29 @@
+package com.jo.lab12sck8scaller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("home")
+@EnableConfigurationProperties(MySQLConfig.class)
+public class HomeController {
+
+    @Autowired
+    private MySQLConfig mySQLConfig;
+
+    @GetMapping
+    public String hello(@PathParam("name") String name){
+        return "hello "+name;
+    }
+
+    @GetMapping("mysql-config")
+    public String getMySqlConfig(){
+        return mySQLConfig.toString();
+    }
+
+}
---
+++ lab-12-sc-k8s/lab-12-sc-k8s-caller/src/test/java/com/jo/lab12sck8scaller/Lab12ScK8sServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jo.lab12sck8scaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class Lab12ScK8sServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
